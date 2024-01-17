64170f53f68be4173b370e99adedae89005eef4c
yuxin <yuxin@microsoft.com>
2024-01-17 16:39:47 +0000

    add code comment

--- app/src/main/java/com/azuresamples/msaldelegatedandroidkotlinsampleapp/MainActivity.kt
+++ app/src/main/java/com/azuresamples/msaldelegatedandroidkotlinsampleapp/MainActivity.kt
@@ -14,18 +14,22 @@ import com.microsoft.identity.client.IAuthenticationResult
 import com.microsoft.identity.client.IMultipleAccountPublicClientApplication
 import com.microsoft.identity.client.PublicClientApplication
 import com.microsoft.identity.client.SilentAuthenticationCallback
+import com.microsoft.identity.client.exception.MsalClientException
 import com.microsoft.identity.client.exception.MsalException
+import com.microsoft.identity.client.exception.MsalServiceException
+import com.microsoft.identity.client.exception.MsalUiRequiredException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+    /* Azure AD Variables */
     private lateinit var authClient: IMultipleAccountPublicClientApplication
     private lateinit var accountList: List<IAccount>
     private var accessToken: String? = null
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding!!
 
     companion object {
         private val TAG = MainActivity::class.java.simpleName
@@ -38,6 +42,8 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        /* Creates a PublicClientApplication object and load currently signed-in accounts. */
         CoroutineScope(Dispatchers.Main).launch {
             authClient = initClient()
             accountList = getAccounts()
@@ -47,6 +53,9 @@ class MainActivity : AppCompatActivity() {
         initializeButtonListeners()
     }
 
+    /**
+     * Initializes UI button listeners.
+     */
     private fun initializeButtonListeners() {
         binding.btnAcquireTokenInteractively.setOnClickListener {
             acquireTokenInteractively()
@@ -65,9 +74,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Acquire token interactively. It will also create an account object for the silent call as a result (to be obtained by getAccount()).
+     *
+     * If acquireTokenSilent() returns an error that requires an interaction,
+     * invoke acquireToken() to have the user resolve the interrupt interactively.
+     *
+     * Some example scenarios are
+     *  - password change
+     *  - the resource you're acquiring a token for has a stricter set of requirement than your SSO refresh token.
+     *  - you're introducing a new scope which the user has never consented for.
+     */
     private fun acquireTokenInteractively() {
         binding.txtLog.text = ""
 
+        /* Extracts a scope array from a text field, i.e. from "User.Read User.ReadWrite" to ["user.read", "user.readwrite"] */
         val scopes = binding.scope.text.toString().lowercase().split(" ")
         val parameters = AcquireTokenParameters.Builder()
             .startAuthorizationFromActivity(this@MainActivity)
@@ -78,9 +99,16 @@ class MainActivity : AppCompatActivity() {
         authClient.acquireToken(parameters)
     }
 
+    /**
+     * Performs acquireToken without interrupting the user.
+     *
+     * This requires an account object of the account you're obtaining a token for.
+     * (can be obtained via getAccount()).
+     */
     private fun acquireTokenSilently() {
         binding.txtLog.text = ""
 
+        /* Extracts a scope array from a text field, i.e. from "User.Read User.ReadWrite" to ["user.read", "user.readwrite"] */
         val scopes = binding.scope.text.toString().lowercase().split(" ")
         val selectedAccount: IAccount = accountList[binding.accountList.selectedItemPosition]
         val parameters = AcquireTokenSilentParameters.Builder()
@@ -94,6 +122,9 @@ class MainActivity : AppCompatActivity() {
         authClient.acquireTokenSilentAsync(parameters)
     }
 
+    /**
+     * Removes the selected account and cached tokens from this app (or device, if the device is in shared mode).
+     */
     private fun removeAccount() {
         binding.txtLog.text = ""
 
@@ -122,6 +153,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Callback used in for silent acquireToken calls.
+     */
     private fun getAuthInteractiveCallback(): AuthenticationCallback {
         return object : AuthenticationCallback {
 
@@ -146,19 +180,32 @@ class MainActivity : AppCompatActivity() {
 
                 accessToken = null
                 binding.txtLog.text = getString(R.string.exception_authentication) + exception
+
+                if (exception is MsalClientException) {
+                    /* Exception inside MSAL, more info inside MsalError.java */
+                } else if (exception is MsalServiceException) {
+                    /* Exception when communicating with the STS, likely config issue */
+                }
             }
 
             override fun onCancel() {
-                // Do nothing
+                /* User canceled the authentication */
+                Log.d(TAG, "User cancelled login.");
             }
         }
     }
 
+    /**
+     * Callback used for interactive request.
+     * If succeeds, we display the access token
+     * Does not check cache.
+     */
     private fun getAuthSilentCallback(): SilentAuthenticationCallback {
         return object : SilentAuthenticationCallback {
             override fun onSuccess(authenticationResult: IAuthenticationResult?) {
                 Log.d(TAG, "Successfully authenticated")
 
+                /* Display Access Token */
                 accessToken = authenticationResult?.accessToken
                 binding.txtLog.text = getString(R.string.log_token_silent) + accessToken
             }
@@ -169,14 +216,30 @@ class MainActivity : AppCompatActivity() {
 
                 accessToken = null
                 binding.txtLog.text = getString(R.string.exception_authentication) + exception
+
+                when (exception) {
+                    is MsalClientException -> {
+                        /* Exception inside MSAL, more info inside MsalError.java */
+                    }
+                    is MsalServiceException -> {
+                        /* Exception when communicating with the STS, likely config issue */
+                    }
+                    is MsalUiRequiredException -> {
+                        /* Tokens expired or no session, retry with interactive */
+                    }
+                }
             }
 
         }
     }
 
+    /**
+     * Callback used in for removing accounts from cache.
+     */
     private fun removeAccountCallback(): IMultipleAccountPublicClientApplication.RemoveAccountCallback {
         return object : IMultipleAccountPublicClientApplication.RemoveAccountCallback {
             override fun onRemoved() {
+                /* Reload account asynchronously to get the up-to-date list. */
                 CoroutineScope(Dispatchers.Main).launch {
                     accessToken = null
                     accountList = getAccounts()
@@ -194,15 +257,28 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * Creates a PublicClientApplication object with res/raw/auth_config_ciam.json
+     */
     private suspend fun initClient(): IMultipleAccountPublicClientApplication = withContext(Dispatchers.IO) {
         return@withContext PublicClientApplication.createMultipleAccountPublicClientApplication(
             this@MainActivity,
             R.raw.auth_config_ciam
         )
     }
+
+    /**
+     * Load currently signed-in accounts, if there's any.
+     */
     private suspend fun getAccounts(): List<IAccount> = withContext(Dispatchers.IO) {
         return@withContext authClient.accounts
     }
+
+    //
+    // Helper methods manage UI updates
+    // ================================
+    // updateUI() - Updates UI based on account list
+    //
 
     private fun updateUI(accounts : List<IAccount>) {
         if (accounts.isNotEmpty()) {
